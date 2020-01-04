88a969b8ba3e0c58aca5688c3859ceaba59bcb95
Viktor Vasatko <viktor.vasatko@seznam.cz>
2020-01-04 14:49:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,119 @@
+# IDS-2018
+
+**classes:**
+  * Benign
+  * Bot
+  * Infilteration
+  * SQL Injection
+  * Brute Force -Web
+  * Brute Force -XSS
+  * FTP-BruteForce
+  * SSH-Bruteforce
+  * DDOS attack-HOIC
+  * DDOS attack-LOIC-UDP
+  * DDoS attacks-LOIC-HTTP
+  * DoS attacks-Hulk
+  * DoS attacks-SlowHTTPTest
+  * DoS attacks-Slowloris
+  * DoS attacks-GoldenEye
+
+**files**:
+
+* Friday-02-03-2018_TrafficForML_CICFlowMeter.csv (Benign, Bot)
+* Friday-16-02-2018_TrafficForML_CICFlowMeter.csv (Benign, DoS attacks-SlowHTTPTest, DoS attacks-Hulk)
+* Friday-23-02-2018_TrafficForML_CICFlowMeter.csv (Benign, SQL Injection, Brute Force -XSS, Brute Force -Web)
+* Thuesday-20-02-2018_TrafficForML_CICFlowMeter.csv (Benign, DDoS attacks-LOIC-HTTP)
+* Thursday-01-03-2018_TrafficForML_CICFlowMeter.csv (Benign, Infilteration)
+* Thursday-15-02-2018_TrafficForML_CICFlowMeter.csv (Benign, DoS attacks-GoldenEye, DoS attacks-Slowloris)
+* Thursday-22-02-2018_TrafficForML_CICFlowMeter.csv (Benign, SQL Injection, Brute Force -XSS, Brute Force -Web)
+* Wednesday-14-02-2018_TrafficForML_CICFlowMeter.csv (Benign, FTP-BruteForce, SSH-Bruteforce)
+* Wednesday-21-02-2018_TrafficForML_CICFlowMeter.csv (Benign, DDOS attack-HOIC, DDOS attack-LOIC-UDP)
+
+**headers**:
+
+* all except thuesday file:
+  * Dst Port
+  * Protocol
+  * Timestamp
+  * Flow Duration
+  * Tot Fwd Pkts
+  * Tot Bwd Pkts
+  * TotLen Fwd Pkts
+  * TotLen Bwd Pkts
+  * Fwd Pkt Len Max
+  * Fwd Pkt Len Min
+  * Fwd Pkt Len Mean
+  * Fwd Pkt Len Std
+  * Bwd Pkt Len Max
+  * Bwd Pkt Len Min
+  * Bwd Pkt Len Mean
+  * Bwd Pkt Len Std
+  * Flow Byts/s
+  * Flow Pkts/s
+  * Flow IAT Mean
+  * Flow IAT Std
+  * Flow IAT Max
+  * Flow IAT Min
+  * Fwd IAT Tot
+  * Fwd IAT Mean
+  * Fwd IAT Std
+  * Fwd IAT Max
+  * Fwd IAT Min
+  * Bwd IAT Tot
+  * Bwd IAT Mean
+  * Bwd IAT Std
+  * Bwd IAT Max
+  * Bwd IAT Min
+  * Fwd PSH Flags
+  * Bwd PSH Flags
+  * Fwd URG Flags
+  * Bwd URG Flags
+  * Fwd Header Len
+  * Bwd Header Len
+  * Fwd Pkts/s
+  * Bwd Pkts/s
+  * Pkt Len Min
+  * Pkt Len Max
+  * Pkt Len Mean
+  * Pkt Len Std
+  * Pkt Len Var
+  * FIN Flag Cnt
+  * SYN Flag Cnt
+  * RST Flag Cnt
+  * PSH Flag Cnt
+  * ACK Flag Cnt
+  * URG Flag Cnt
+  * CWE Flag Count
+  * ECE Flag Cnt
+  * Down/Up Ratio
+  * Pkt Size Avg
+  * Fwd Seg Size Avg
+  * Bwd Seg Size Avg
+  * Fwd Byts/b Avg
+  * Fwd Pkts/b Avg
+  * Fwd Blk Rate Avg
+  * Bwd Byts/b Avg
+  * Bwd Pkts/b Avg
+  * Bwd Blk Rate Avg
+  * Subflow Fwd Pkts
+  * Subflow Fwd Byts
+  * Subflow Bwd Pkts
+  * Subflow Bwd Byts
+  * Init Fwd Win Byts
+  * Init Bwd Win Byts
+  * Fwd Act Data Pkts
+  * Fwd Seg Size Min
+  * Active Mean
+  * Active Std
+  * Active Max
+  * Active Min
+  * Idle Mean
+  * Idle Std
+  * Idle Max
+  * Idle Min
+  * Label
+* thuesday file header has two more 4 more columns:
+  * Flow ID
+  * Src IP
+  * Src Port
+  * Dst IP
---
+++ constants.py
@@ -0,0 +1,142 @@
+from os import sep
+
+DATA_DIR = "data_csv"
+
+CONF_DIR = "conf"
+
+PATH_TO_DATA = ".." + sep + DATA_DIR
+
+ALL_ORIGINAL_FILES = [
+     "Friday-02-03-2018_TrafficForML_CICFlowMeter.csv",
+     "Friday-16-02-2018_TrafficForML_CICFlowMeter.csv",
+     "Friday-23-02-2018_TrafficForML_CICFlowMeter.csv",
+     "Thuesday-20-02-2018_TrafficForML_CICFlowMeter.csv",
+     "Thursday-01-03-2018_TrafficForML_CICFlowMeter.csv",
+     "Thursday-15-02-2018_TrafficForML_CICFlowMeter.csv",
+     "Thursday-22-02-2018_TrafficForML_CICFlowMeter.csv",
+     "Wednesday-14-02-2018_TrafficForML_CICFlowMeter.csv",
+     "Wednesday-21-02-2018_TrafficForML_CICFlowMeter.csv",
+     "Wednesday-28-02-2018_TrafficForML_CICFlowMeter.csv"
+]
+
+ORIGINAL_FILES_WITH_SAME_HEADER = [
+    [
+        "Friday-02-03-2018_TrafficForML_CICFlowMeter.csv",
+        "Friday-16-02-2018_TrafficForML_CICFlowMeter.csv",
+        "Friday-23-02-2018_TrafficForML_CICFlowMeter.csv",
+        "Thursday-01-03-2018_TrafficForML_CICFlowMeter.csv",
+        "Thursday-15-02-2018_TrafficForML_CICFlowMeter.csv",
+        "Thursday-22-02-2018_TrafficForML_CICFlowMeter.csv",
+        "Wednesday-14-02-2018_TrafficForML_CICFlowMeter.csv",
+        "Wednesday-21-02-2018_TrafficForML_CICFlowMeter.csv",
+        "Wednesday-28-02-2018_TrafficForML_CICFlowMeter.csv"
+    ],
+    [
+        "Thuesday-20-02-2018_TrafficForML_CICFlowMeter.csv"
+    ]
+]
+
+SPLIT_FILES = [
+    "Friday-02-03-2018_TrafficForML_CICFlowMeter_0.csv",
+    "Friday-16-02-2018_TrafficForML_CICFlowMeter_0.csv",
+    "Friday-16-02-2018_TrafficForML_CICFlowMeter_1.csv",
+    "Friday-23-02-2018_TrafficForML_CICFlowMeter_0.csv",
+    "Thuesday-20-02-2018_TrafficForML_CICFlowMeter_dropped_0.csv",
+    "Thursday-01-03-2018_TrafficForML_CICFlowMeter_0.csv",
+    "Thursday-01-03-2018_TrafficForML_CICFlowMeter_1.csv",
+    "Thursday-01-03-2018_TrafficForML_CICFlowMeter_2.csv",
+    "Thursday-01-03-2018_TrafficForML_CICFlowMeter_3.csv",
+    "Thursday-01-03-2018_TrafficForML_CICFlowMeter_4.csv",
+    "Thursday-01-03-2018_TrafficForML_CICFlowMeter_5.csv",
+    "Thursday-01-03-2018_TrafficForML_CICFlowMeter_6.csv",
+    "Thursday-01-03-2018_TrafficForML_CICFlowMeter_7.csv",
+    "Thursday-01-03-2018_TrafficForML_CICFlowMeter_8.csv",
+    "Thursday-01-03-2018_TrafficForML_CICFlowMeter_9.csv",
+    "Thursday-01-03-2018_TrafficForML_CICFlowMeter_10.csv",
+    "Thursday-01-03-2018_TrafficForML_CICFlowMeter_11.csv",
+    "Thursday-01-03-2018_TrafficForML_CICFlowMeter_12.csv",
+    "Thursday-01-03-2018_TrafficForML_CICFlowMeter_13.csv",
+    "Thursday-01-03-2018_TrafficForML_CICFlowMeter_14.csv",
+    "Thursday-01-03-2018_TrafficForML_CICFlowMeter_15.csv",
+    "Thursday-01-03-2018_TrafficForML_CICFlowMeter_16.csv",
+    "Thursday-01-03-2018_TrafficForML_CICFlowMeter_17.csv",
+    "Thursday-01-03-2018_TrafficForML_CICFlowMeter_18.csv",
+    "Thursday-01-03-2018_TrafficForML_CICFlowMeter_19.csv",
+    "Thursday-01-03-2018_TrafficForML_CICFlowMeter_20.csv",
+    "Thursday-01-03-2018_TrafficForML_CICFlowMeter_21.csv",
+    "Thursday-01-03-2018_TrafficForML_CICFlowMeter_22.csv",
+    "Thursday-01-03-2018_TrafficForML_CICFlowMeter_23.csv",
+    "Thursday-01-03-2018_TrafficForML_CICFlowMeter_24.csv",
+    "Thursday-01-03-2018_TrafficForML_CICFlowMeter_25.csv",
+    "Thursday-15-02-2018_TrafficForML_CICFlowMeter_0.csv",
+    "Thursday-22-02-2018_TrafficForML_CICFlowMeter_0.csv",
+    "Wednesday-14-02-2018_TrafficForML_CICFlowMeter_0.csv",
+    "Wednesday-21-02-2018_TrafficForML_CICFlowMeter_0.csv",
+    "Wednesday-28-02-2018_TrafficForML_CICFlowMeter_0.csv",
+    "Wednesday-28-02-2018_TrafficForML_CICFlowMeter_1.csv",
+    "Wednesday-28-02-2018_TrafficForML_CICFlowMeter_2.csv",
+    "Wednesday-28-02-2018_TrafficForML_CICFlowMeter_3.csv",
+    "Wednesday-28-02-2018_TrafficForML_CICFlowMeter_4.csv",
+    "Wednesday-28-02-2018_TrafficForML_CICFlowMeter_5.csv",
+    "Wednesday-28-02-2018_TrafficForML_CICFlowMeter_6.csv",
+    "Wednesday-28-02-2018_TrafficForML_CICFlowMeter_7.csv",
+    "Wednesday-28-02-2018_TrafficForML_CICFlowMeter_8.csv",
+    "Wednesday-28-02-2018_TrafficForML_CICFlowMeter_9.csv",
+    "Wednesday-28-02-2018_TrafficForML_CICFlowMeter_10.csv",
+    "Wednesday-28-02-2018_TrafficForML_CICFlowMeter_11.csv",
+    "Wednesday-28-02-2018_TrafficForML_CICFlowMeter_12.csv",
+    "Wednesday-28-02-2018_TrafficForML_CICFlowMeter_13.csv",
+    "Wednesday-28-02-2018_TrafficForML_CICFlowMeter_14.csv",
+    "Wednesday-28-02-2018_TrafficForML_CICFlowMeter_15.csv",
+    "Wednesday-28-02-2018_TrafficForML_CICFlowMeter_16.csv",
+    "Wednesday-28-02-2018_TrafficForML_CICFlowMeter_17.csv",
+    "Wednesday-28-02-2018_TrafficForML_CICFlowMeter_18.csv",
+    "Wednesday-28-02-2018_TrafficForML_CICFlowMeter_19.csv",
+    "Wednesday-28-02-2018_TrafficForML_CICFlowMeter_20.csv",
+    "Wednesday-28-02-2018_TrafficForML_CICFlowMeter_21.csv",
+    "Wednesday-28-02-2018_TrafficForML_CICFlowMeter_22.csv",
+    "Wednesday-28-02-2018_TrafficForML_CICFlowMeter_23.csv",
+    "Wednesday-28-02-2018_TrafficForML_CICFlowMeter_24.csv",
+    "Wednesday-28-02-2018_TrafficForML_CICFlowMeter_25.csv",
+    "Wednesday-28-02-2018_TrafficForML_CICFlowMeter_26.csv",
+    "Wednesday-28-02-2018_TrafficForML_CICFlowMeter_27.csv",
+    "Wednesday-28-02-2018_TrafficForML_CICFlowMeter_28.csv",
+    "Wednesday-28-02-2018_TrafficForML_CICFlowMeter_29.csv",
+    "Wednesday-28-02-2018_TrafficForML_CICFlowMeter_30.csv",
+    "Wednesday-28-02-2018_TrafficForML_CICFlowMeter_31.csv",
+    "Wednesday-28-02-2018_TrafficForML_CICFlowMeter_32.csv",
+    "Wednesday-28-02-2018_TrafficForML_CICFlowMeter_33.csv"
+]
+
+FULL_ANALYSIS_CONF = "full_analysis.yaml"
+
+CLASSES_ANALYSIS_CONF = "classes_analysis.yaml"
+
+ANALYSIS_BY_PARTS = [
+    "analysis_part_1.yaml",
+    "analysis_part_2.yaml",
+    "analysis_part_3.yaml",
+    "analysis_part_4.yaml",
+    "analysis_part_5.yaml",
+    "analysis_part_6.yaml",
+    "analysis_part_7.yaml",
+    "analysis_part_8.yaml",
+]
+
+CLASSES = [
+    "Benign",
+    "Bot",
+    "Infilteration",
+    "SQL Injection",
+    "Brute Force -Web",
+    "Brute Force -XSS",
+    "FTP-BruteForce",
+    "SSH-Bruteforce",
+    "DDOS attack-HOIC",
+    "DDOS attack-LOIC-UDP",
+    "DDoS attacks-LOIC-HTTP",
+    "DoS attacks-Hulk",
+    "DoS attacks-SlowHTTPTest",
+    "DoS attacks-Slowloris",
+    "DoS attacks-GoldenEye"
+]
